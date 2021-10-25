bf72336fa03fa39c47501860d61e5a4f72e9d8c3
Asaf Cohen <safhac@gamil.com>
2021-10-25 13:41:10 +0300

    refactor update as a class and an fsm

--- client/update.py
+++ client/update.py
@@ -2,23 +2,38 @@ from model import Model
 from model import Msg
 
 
-async def update(model: Model, msg: Msg) -> (Model, Msg):
-    match msg:
-        case Msg.Listen:
-            print(f'listen {model} {msg}')
-            await listen(model)
-        case Msg.Process:
-            print(f'process {model} {msg}')
+class Update:
+    def __init__(self, model, msg):
+        self._model = model
+        self._msg = msg
 
-        case Msg.Nothing:
-            print(f'Nothing {model} {msg}')
-            return (Model.Listen, Msg.Nothing)
-        case _:
-            return (Model.Listen, Msg.Nothing)
+
+    async def update(self, *args, **kwargs):
+        self._msg = yield
+        match self._msg:
+            case Msg.Listen:
+                print('listen')
+                await listen(self._model)
+            case Msg.Process:
+                print('process')
+                await listen(self._model)
+            case Msg.Send:
+                print('send')
+                await listen(self._model)
+            case Msg.Failure:
+                print('failure')
+                await listen(self._model)
+            case Msg.NoOp:
+                print('noop')
+                await listen(self._model)
+            case _:
+                print('_')
+                await listen(self._model)
 
 
 async def listen(model):
-    await update(model, Msg.Process)
+    print('listen function')
+    await Update(model, Msg.Process)
 
 
 async def process(): ...
