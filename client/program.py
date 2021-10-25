fedad0137750a5783db956ecb53b0e2563202f0a
Asaf Cohen <safhac@gamil.com>
2021-10-25 13:42:06 +0300

    add docstring and fix broken imports

--- client/program.py
+++ client/program.py
@@ -1,9 +1,21 @@
+from .update import Update
+from .model import Model
+from .model import Msg
+
 
 class Program:
+    """
+    The class represents the program primary parts
+    Initial state, Update method, Subscription method
+    The program is initiated with init initial state, the update function and the subscriptions
+    event listener function.
+    """
 
-    def __init__(self, initial_data, update, subscriptions):
-        self.init = initial_data()
-        self.update = update
+    def __init__(self, init: (Model, Msg), update: Update, subscriptions):
+        model, msg = init()
+        self.update = update(model, msg)
         self.subscriptions = subscriptions
 
-
+    async def run(self):
+        while True:
+            self.update.update()
