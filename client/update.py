5deef109b8b7fd95e7b4bd5196878f8fba6834bd
Asaf Cohen <safhac@gamil.com>
2021-11-10 14:26:27 +0200

    move actions out of update.py

--- client/update.py
+++ client/update.py
@@ -1,48 +1,11 @@
-import time
-import asyncio
 from datetime import datetime
 
 from client.model import Model
 from client.model import DataStatus
 from client.model import Msg
-from client.model import PORT
-from client.model import HOST
 
-from managers import Listen
-
-
-async def listen():
-    try:
-        reader, writer = await asyncio.open_connection(
-            HOST, PORT)
-
-    except ConnectionRefusedError as e:
-        print('connection error')
-        # log error
-        print(e.args)
-        return (None, e.args[1])
-
-    else:
-        data = await reader.read(100)
-        message = data.decode()
-        addr = writer.get_extra_info('peername')
-
-        print(f"Received {message!r} from {addr!r}")
-
-    finally:
-        print('listen complete')
-
-
-async def process():
-    ...
-
-
-async def send():
-    ...
-
-
-async def failure():
-    ...
+from managers import Manager
+from managers import listen
 
 
 class Update:
@@ -65,17 +28,20 @@ class Update:
                 state = DataStatus(False, datetime.now())
                 self._model = Model(state)
                 print('listen')
-                with Listen():
 
-                    data, error = await listen()
+                with Manager(Msg.Listen) as manager:
+                    data, error = await listen(manager)
+
                 if data:
                     self._model = Model(state, data)
                     await self.update(Msg.Process)
+
                 elif error:
                     self._model = Model(state, error)
                     await self.update(Msg.Failure)
 
             case Msg.Process:
+
                 print('process')
                 print(self._model)
             case Msg.Send:
