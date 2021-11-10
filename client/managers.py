d7048e9cca01756312d1a51a0d27d30541746ed0
Asaf Cohen <safhac@gamil.com>
2021-11-10 14:25:26 +0200

    move update actions to managers.py

--- client/managers.py
+++ client/managers.py
@@ -1,7 +1,65 @@
+import os
+import asyncio
 from contextlib import AbstractContextManager
+from model import Msg
+from model import PORT
+from model import HOST
 
 
-class Listen(AbstractContextManager):
+async def listen(self):
+    try:
+        reader, writer = await asyncio.open_connection(
+            HOST, PORT)
+
+    except ConnectionRefusedError as e:
+        print('connection error')
+        # log error
+        print(e.args)
+        return (None, e.args[1])
+
+    else:
+        data = await reader.read(100)
+        message = data.decode()
+        addr = writer.get_extra_info('peername')
+
+        print(f"Received {message!r} from {addr!r}")
+
+    finally:
+        print('listen complete')
+
+
+async def process():
+    ...
+
+
+async def send():
+    ...
+
+
+async def failure():
+    ...
+
+
+action_map = dict(
+    zip(
+        list(Msg.__members__)[:-1],
+        [listen, process, send, failure]
+    ))
+
+
+class Manager(AbstractContextManager):
+
+    def __init__(self, context: Msg):
+
+        self.host = HOST
+
+        try:
+            self.port = os.environ['PORT']
+
+        except KeyError:
+            # log error
+            print('cant find port')
+            self.port = PORT
 
     def __enter__(self):
         print(f'enter listen')
@@ -16,3 +74,8 @@ class Listen(AbstractContextManager):
             return True
         return False
 
+    def run(self, *args, **kwargs):
+        return self.action()
+
+    # def action(self):
+    #     return self.action()
