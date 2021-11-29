fe901c7f32243e60eb0b63770f5058889f5526e1
Asaf Cohen <safhac@gamil.com>
2021-11-29 11:42:07 +0200

    adding setup

---
+++ server/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = ['asyncio']
+
+setup(
+    name='server',
+    author='',
+    author_email='',
+    version='0.0.3',
+    description='Listen for socket events',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'scripts']),
+    entry_points={
+        'console_scripts': [
+            'server = server.main: main'
+        ],
+    },
+    install_requires=install_requires,
+    test_suit='tests',
+    zip_safe=False)
